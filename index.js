43f785d9c2758ff0a9d083790911b3393a1deb9a
bmeudy11 <bmeudy@gmail.com>
2025-09-07 20:47:14 -0400

    correct model key value

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 //init Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({mode: "gemini-2.5-Pro"});
+const model = genAI.getGenerativeModel({model: "gemini-2.5-Pro"});
 
 const app = express();
 const port = 3002;
